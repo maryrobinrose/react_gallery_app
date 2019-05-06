04134da7726caed43355ed988ae98e26d5c99aa2
Mary Robin Rose <maryrobinrose@gmail.com>
2019-05-06 16:00:56 -0700

    add video notes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,62 @@
-import React from 'react';
+import React, { Component }from 'react';
 import './App.css';
+import Header from './Header';
+import Player from './Player';
 
-function App() {
-  return (
-    <div>
+class App extends Component {
+  state = {
+      players: [
+        {
+        name: "Guil",
+        score: 50,
+        id: 1
+      },
+      {
+        name: "Treasure",
+        score: 85,
+        id: 2
+      },
+      {
+        name: "Ashley",
+        score: 95,
+        id: 3
+      },
+      {
+        name: "James",
+        score: 80,
+        id: 4
+      }
+    ]
+  }:
 
+  //Remove Items from State
+  handleRemovePlayer = (id) => {
+    this.setState( prevState => {
+      return {
+        players: prevState.players.filter( p => p.id !== id)
+      };
+    });
+  }
 
-    </div>
+  render() {
+    return (
+      <div className='scoreboard'>
+        <Header
+          title="Scoreboard"
+          totalPlayers={props.initialyPlayers.length}
+        />
+
+        {/* Players list */}
+        //map loops over players
+        {this.state.players.map( player =>
+          <Player
+            name={player.nae}
+            id={player.id}
+            key={player.id.toString()}
+            removePlayer={this.handleRemovePlayer}
+          />
+        )}
+      </div>
   );
 }
 
