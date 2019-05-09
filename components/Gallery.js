63b9d2b6391f75cf7c2c4106c1209bbfb399f51f
Mary Robin Rose <maryrobinrose@gmail.com>
2019-05-08 21:13:15 -0700

    add changes

--- components/Gallery.js
+++ components/Gallery.js
@@ -1,1 +1,11 @@
 //can be reused to display the sets of images for each of the three topic categories you wish to display, like sunsets, waterfalls and rainbows, for example.
+
+import React from 'react';
+
+const Gallery = props => (
+  <li className="gif-wrap">
+    <img src={props.url} alt=""/>
+  </li>
+);
+
+export default Gallery;
--- src/App.js
+++ src/App.js
@@ -5,20 +5,23 @@ import {
   Route,
   Link
 } from 'react-router-dom';
-
-//Connect to App Components
-import { Provider } from './Context';
 import './App.css';
 import axios from 'axios';
+import apiKey from 'config.js';
+
+//Connect to App Components
 import Header from './Header';
 import Gallery from './Gallery';
 import Search from './Search';
-import apiKey from 'config.js';
+
+//API key variable
+const flickrApi = apiKey;
 
 export default class App extends Component {
   constructor() {
     super();
     this.state = {
+      images: [],
       sunsets: [],
       waterfalls: [],
       rainbows: [],
@@ -27,14 +30,15 @@ export default class App extends Component {
   }
 
   componentDidMount() {
-    this.performSearch('sunsets'); //?
-
+    this.performSearch();
+    this.performSearch('sunsets');
+    this.performSearch('waterfalls');
+    this.performSearch('rainbows');
   }
 
 
   performSearch = (query = 'sunsets') => {
-    //change to flickr
-    axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=a76b7db2a7dfe0204f80447c3b43c5af&tags=${query}&per_page=&format=json&nojsoncallback=1`)
+    axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${key}&tags=${query}&per_page=&format=json&nojsoncallback=1`)
       .then(response => {
         this.setState({
           images: response.data.data,
@@ -46,19 +50,26 @@ export default class App extends Component {
       });
   }
 
-const App = () => {
-
-      return (
-        //Renders route router that listens to URL changes
-        <BrowserRouter>
-          <div className='scoreboard'>
-            <Header />
-            <Route exact path="/" component={Home} />
-            <AddPlayerForm />
+  render() {
+    return (
+      <div>
+        <div className="main-header">
+          <div className="inner">
+            <h1 className="main-title">GifSearch</h1>
+            <SearchForm onSearch={this.performSearch}/>
           </div>
-        </BrowserRouter>
+        </div>
+        <div className="main-content">
+          {
+            (this.state.loading)
+            ? <p>Loading...</p>
+            : <GifList data={this.state.gifs}/>
+          }
+        </div>
+      </div>
     );
   }
+
 }
 
 export default App;
