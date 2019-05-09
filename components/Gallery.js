490af0d00a18455b708239cc1be1177eff7215ae
Mary Robin Rose <maryrobinrose@gmail.com>
2019-05-08 21:16:29 -0700

    change class name

--- components/Gallery.js
+++ components/Gallery.js
@@ -1,9 +1,7 @@
-//can be reused to display the sets of images for each of the three topic categories you wish to display, like sunsets, waterfalls and rainbows, for example.
-
 import React from 'react';
 
 const Gallery = props => (
-  <li className="gif-wrap">
+  <li className="photo-container ">
     <img src={props.url} alt=""/>
   </li>
 );
