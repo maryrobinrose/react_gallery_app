f8469677fe86acad716e3e55a7428f9896d3d85a
Mary Robin Rose <maryrobinrose@gmail.com>
2019-05-10 15:28:29 -0700

    switch file material

--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -1,11 +1,27 @@
 //Import React library
 import React from 'react';
+import Gallery from './Gallery';
+import NoImage from './NoImage';
 
-const Gallery = props => (
-  <li className="photo-container ">
-    <img src={props.url} alt=""/>
-  </li>
-);
+const GalleryItem = props => {
 
-//Expose Gallery component to other modules
-export default Gallery;
+  const results = props.data;
+  let images;
+  if(results.length > 0) {
+    images = results.map(image => <Gallery url={} key={image.id}/>);
+  } else {
+    images = <NoImage />
+  }
+
+
+  return(
+    <div className="photo-container">
+      <h1>Results</h1>
+        <ul>
+          {images}
+        </ul>
+    </div>
+  );
+}
+
+export default GalleryItem;
--- src/Components/GalleryItem.js
+++ src/Components/GalleryItem.js
@@ -1,27 +1,11 @@
 //Import React library
 import React from 'react';
-import Gallery from './Gallery';
-import NoImage from './NoImage';
 
-const GalleryItem = props => {
+const Gallery = props => (
+  <li className="photo-container ">
+    <img src={props.url} alt=""/>
+  </li>
+);
 
-  const results = props.data;
-  let images;
-    if(results.length > 0) {
-      images = results.map(image => <Gallery url={} key={image.id}/>);
-    } else {
-      images = <NoImage />
-    }
-
-
-  return(
-    <div className="photo-container">
-      <h1>Results</h1>
-        <ul>
-          {images}
-        </ul>
-    </div>
-  );
-}
-
-export default GalleryItem;
+//Expose Gallery component to other modules
+export default Gallery;
