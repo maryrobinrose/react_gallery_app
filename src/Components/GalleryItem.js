211be78b75ca7ca58685554ac8862e845c5e7653
Mary Robin Rose <maryrobinrose@gmail.com>
2019-05-11 17:04:29 -0700

    add li

--- src/Components/GalleryItem.js
+++ src/Components/GalleryItem.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const GalleryItem = props => (
-
+  <li className="photo-container li:hover img">
     <img src={props.url} alt=""/>
-
+  </li>
 );
 
 export default GalleryItem;
